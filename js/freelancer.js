d2f8020a5dc0a7410262008bbc50d9d1157d766a
Jennifer Turner <jenniferturner202@gmail.com>
2014-08-11 11:25:43 -0400

    Created repository

---
+++ js/freelancer.js
@@ -0,0 +1,56 @@
+/*!
+ * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+
+$(function(){
+$('.name').flicker();
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+
+// Floating label headings for the contact form
+$(function() {
+    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
+    }).on("focus", ".floating-label-form-group", function() {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function() {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Flicker for lightblulb effect
+
+$.fn.flicker.defaults = {
+    minOpacity: 0.7,     // Opacity will not go below this value
+    maxOpacity: 1,       // Opacity will not exceed this value
+    transition: 200,     // The speed of the animation
+    delay: 75,           // The time between changes
+    probability: 0.1     // The probability of a flickering event
+};
+
+
+
